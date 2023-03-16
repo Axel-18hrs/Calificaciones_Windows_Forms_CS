49c58a8bd613292abe19c69fc3b11a2ab31e17e0
Axel Gael Carlin Guzman <axel.18hrs@gmail.com>
2023-03-15 22:53:07 -0600

    Agregar archivos de proyecto.

---
+++ Calificaciones_Windows_Forms_CS/Form1.Designer.cs
@@ -0,0 +1,428 @@
+﻿namespace Calificaciones_Windows_Forms_CS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.nud1 = new System.Windows.Forms.NumericUpDown();
+            this.nud2 = new System.Windows.Forms.NumericUpDown();
+            this.nud3 = new System.Windows.Forms.NumericUpDown();
+            this.nud4 = new System.Windows.Forms.NumericUpDown();
+            this.nud5 = new System.Windows.Forms.NumericUpDown();
+            this.nud6 = new System.Windows.Forms.NumericUpDown();
+            this.dgvCalificaciones = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nud1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCalificaciones)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(53, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(202, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Activity 1 and unit 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(53, 156);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(202, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Activity 2 and unit 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(53, 216);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(202, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Activity 3 and unit 3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(53, 276);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(202, 23);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Activity 4 and unit 4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(53, 338);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(202, 23);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Activity 5 and unit 5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(53, 398);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(202, 23);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Activity 6 and unit 6";
+            // 
+            // nud1
+            // 
+            this.nud1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nud1.Location = new System.Drawing.Point(304, 91);
+            this.nud1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nud1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nud1.Name = "nud1";
+            this.nud1.ReadOnly = true;
+            this.nud1.Size = new System.Drawing.Size(197, 32);
+            this.nud1.TabIndex = 6;
+            this.nud1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // nud2
+            // 
+            this.nud2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nud2.Location = new System.Drawing.Point(304, 147);
+            this.nud2.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nud2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nud2.Name = "nud2";
+            this.nud2.ReadOnly = true;
+            this.nud2.Size = new System.Drawing.Size(197, 32);
+            this.nud2.TabIndex = 7;
+            this.nud2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // nud3
+            // 
+            this.nud3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nud3.Location = new System.Drawing.Point(304, 207);
+            this.nud3.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nud3.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nud3.Name = "nud3";
+            this.nud3.ReadOnly = true;
+            this.nud3.Size = new System.Drawing.Size(197, 32);
+            this.nud3.TabIndex = 8;
+            this.nud3.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // nud4
+            // 
+            this.nud4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nud4.Location = new System.Drawing.Point(304, 267);
+            this.nud4.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nud4.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nud4.Name = "nud4";
+            this.nud4.ReadOnly = true;
+            this.nud4.Size = new System.Drawing.Size(197, 32);
+            this.nud4.TabIndex = 9;
+            this.nud4.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // nud5
+            // 
+            this.nud5.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nud5.Location = new System.Drawing.Point(304, 329);
+            this.nud5.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nud5.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nud5.Name = "nud5";
+            this.nud5.ReadOnly = true;
+            this.nud5.Size = new System.Drawing.Size(197, 32);
+            this.nud5.TabIndex = 10;
+            this.nud5.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // nud6
+            // 
+            this.nud6.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nud6.Location = new System.Drawing.Point(304, 389);
+            this.nud6.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nud6.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nud6.Name = "nud6";
+            this.nud6.ReadOnly = true;
+            this.nud6.Size = new System.Drawing.Size(197, 32);
+            this.nud6.TabIndex = 11;
+            this.nud6.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // dgvCalificaciones
+            // 
+            this.dgvCalificaciones.AllowUserToAddRows = false;
+            this.dgvCalificaciones.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Raised;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvCalificaciones.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvCalificaciones.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCalificaciones.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6});
+            this.dgvCalificaciones.Location = new System.Drawing.Point(526, 91);
+            this.dgvCalificaciones.Name = "dgvCalificaciones";
+            this.dgvCalificaciones.ReadOnly = true;
+            this.dgvCalificaciones.RowHeadersVisible = false;
+            this.dgvCalificaciones.RowHeadersWidth = 51;
+            this.dgvCalificaciones.RowTemplate.Height = 24;
+            this.dgvCalificaciones.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvCalificaciones.Size = new System.Drawing.Size(619, 329);
+            this.dgvCalificaciones.TabIndex = 12;
+            this.dgvCalificaciones.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Unit 1";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Unit 2";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 125;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Unit 3";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 125;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Unit 4";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Width = 125;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Unit 5";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            this.Column5.Width = 125;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Unit 6";
+            this.Column6.MinimumWidth = 6;
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            this.Column6.Width = 125;
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnEnviar.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnviar.Location = new System.Drawing.Point(57, 442);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(172, 64);
+            this.btnEnviar.TabIndex = 13;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = false;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(163, 26);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(861, 34);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Califica las unidades del estudiante que creo esta aplicacion";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1196, 533);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.dgvCalificaciones);
+            this.Controls.Add(this.nud6);
+            this.Controls.Add(this.nud5);
+            this.Controls.Add(this.nud4);
+            this.Controls.Add(this.nud3);
+            this.Controls.Add(this.nud2);
+            this.Controls.Add(this.nud1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nud1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCalificaciones)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown nud1;
+        private System.Windows.Forms.NumericUpDown nud2;
+        private System.Windows.Forms.NumericUpDown nud3;
+        private System.Windows.Forms.NumericUpDown nud4;
+        private System.Windows.Forms.NumericUpDown nud5;
+        private System.Windows.Forms.NumericUpDown nud6;
+        private System.Windows.Forms.DataGridView dgvCalificaciones;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Calificaciones_Windows_Forms_CS/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calificaciones_Windows_Forms_CS
+{
+    public partial class Form1 : Form
+    {
+        private BinaryReader reader;
+        private BinaryWriter writer;
+        private StreamReader readerStream;
+        private FileStream fileStream;
+        private OpenFileDialog ofd;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ofd = new OpenFileDialog();
+            ofd.Title = "Calificaciones del alumno";
+            ofd.Filter = "Archivos binarios (*.bin)|*.bin";
+
+            DialogResult opc = MessageBox.Show("Quieres usar un archivo existente?", "???", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
+            if (opc == DialogResult.Yes)
+            {
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+
+                    fileStream = new FileStream(ofd.FileName, FileMode.Open, FileAccess.Read);
+                    reader = new BinaryReader(fileStream);
+
+                    while (reader.BaseStream.Position != reader.BaseStream.Length)
+                    {
+                        double value1 = reader.ReadDouble();
+                        double value2 = reader.ReadDouble();
+                        double value3 = reader.ReadDouble();
+                        double value4 = reader.ReadDouble();
+                        double value5 = reader.ReadDouble();
+                        double value6 = reader.ReadDouble();
+                        dgvCalificaciones.Rows.Add(value1, value2, value3, value4, value5, value6);
+                        btnEnviar.Enabled = false;
+                        MessageBox.Show("No se podra calificar nuevamente si ya se establecio un archivo para este programa", "???", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    }
+                    reader.Close();
+
+                }
+                else
+                {
+                    MessageBox.Show("Error al guardar el archivo", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            
+            DialogResult x = MessageBox.Show("Deseas guardar las calificaciones?", "???", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
+            if (x == DialogResult.Yes)
+            {
+                SaveFileDialog ruta = new SaveFileDialog();
+                ruta.Filter = "Archivos binarios (*.bin)|*.bin";
+                ruta.Title = "Calificaciones del alumno";
+
+                if (ruta.ShowDialog() == DialogResult.Cancel)
+                {
+                    MessageBox.Show("Error al escoger la ruta de archivo. ", "Error", MessageBoxButtons.OK);
+                    return;
+                }
+                FileStream rutad = new FileStream(ruta.FileName, FileMode.Create, FileAccess.Write);
+                writer = new BinaryWriter(rutad);
+                foreach (DataGridViewRow row in dgvCalificaciones.Rows)
+                {
+                    // Itera sobre cada celda en la fila
+                    foreach (DataGridViewCell cell in row.Cells)
+                    {
+                        // Escribe el valor de la celda en el archivo binario
+                        writer.Write(Convert.ToDouble(cell.Value));
+                    }
+                }
+
+                writer.Close();
+                rutad.Close();
+            }
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            dgvCalificaciones.Rows.Add(nud1.Value.ToString(), nud2.Value.ToString(), nud3.Value.ToString(), nud4.Value.ToString(), nud5.Value.ToString(), nud6.Value.ToString());
+            btnEnviar.Enabled = false;
+        }
+    }
+}
